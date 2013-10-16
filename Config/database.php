6a591cde82668e091a685a06186f8f5303791372
Renato Castanheiro <php3@contactonet.com>
2013-10-16 16:16:08 -0300

    removendo biblioteca do cake

---
+++ Config/database.php
@@ -0,0 +1,25 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'net',
+		'password' => 'internet',
+		'database' => 'alphaville_brasilia',
+		'prefix' => '',
+		//'encoding' => 'utf8',
+	);
+
+	public $test = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'indic012_indalph',
+		'password' => '&9pf5q2KBmiD',
+		'database' => 'indic012_alphaville',
+		'prefix' => '',
+		//'encoding' => 'utf8',
+	);
+}
